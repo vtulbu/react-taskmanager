ac77c856e98f4735cec0782304e34f5e85dafe43
Tulbu Vadim <tulbu.vadim@gmail.com>
2023-04-02 20:59:09 +0300

    Create generator of boards mock data

--- src/providers/board/mockData.ts
+++ src/providers/board/mockData.ts
@@ -1,53 +1,20 @@
 import { Board } from './types';
 
-export const boards: Board[] = [
-  {
-    id: 'new',
-    label: 'New',
-    columns: [],
-  },
-  {
-    id: 'delete',
-    label: 'Delete',
-    columns: [
-      {
-        id: 'test1',
-        label: 'Test 1',
-        task: [
-          {
-            id: 'task1',
-            label: 'Task1',
-            description:
-              "We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
-            subTasks: [
-              {
-                id: 'subtask1',
-                label: 'Subtask 1',
-                isDone: true,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        id: 'test2',
-        label: 'Test 2',
-        task: [
-          {
-            id: 'task2',
-            label: 'Task2',
-            description:
-              "We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
-            subTasks: [
-              {
-                id: 'subtask2',
-                label: 'Subtask 2',
-                isDone: false,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
+export const boards: Board[] = Array.from({ length: 10 }, (_, i) => ({
+  id: `board-${i}`,
+  label: `Board ${i}`,
+  columns: Array.from({ length: 6 }, (_, j) => ({
+    id: `column-${i}-${j}`,
+    label: `Column ${j}`,
+    task: Array.from({ length: 6 }, (_, k) => ({
+      id: `task-${i}-${j}-${k}`,
+      label: `Task ${k}`,
+      description: `Description ${k}: We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.`,
+      subTasks: Array.from({ length: 5 }, (_, l) => ({
+        id: `subtask-${i}-${j}-${k}-${l}`,
+        label: `Subtask ${l}`,
+        isDone: l % 2 === 0,
+      })),
+    })),
+  })),
+}));
