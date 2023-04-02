f3689ff87c45face331aab1f46df60aba6e6e1ef
Tulbu Vadim <tulbu.vadim@gmail.com>
2023-04-02 20:14:40 +0300

    Add loader spinner for loading content listener

--- src/components/Layout/Layout.module.css.d.ts
+++ src/components/Layout/Layout.module.css.d.ts
@@ -1,11 +1,11 @@
 declare const styles: {
-  readonly "layoutContainer": string;
-  readonly "content": string;
-  readonly "contentIsSidebarOpen": string;
-  readonly "contentBackgroundDark": string;
-  readonly "contentBackgroundLight": string;
-  readonly "sidebarButton": string;
-  readonly "p-button-icon-only": string;
+  readonly layoutContainer: string;
+  readonly content: string;
+  readonly contentIsSidebarOpen: string;
+  readonly contentBackgroundDark: string;
+  readonly contentBackgroundLight: string;
+  readonly sidebarButton: string;
+  readonly 'p-button-icon-only': string;
+  readonly 'loading-spinner': string;
 };
 export = styles;
-
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
+import { ProgressSpinner } from 'primereact/progressspinner';
 import { useThemeProvider } from 'src/providers/theme/ThemeProvider';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useSidebar } from 'src/providers/sidebar/SidebarProvider';
 import { Button } from '../Button';
 import { Header } from '../Header';
@@ -9,6 +10,7 @@ import s from './Layout.module.css';
 import { useScreenSize } from 'src/hooks';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  const [loaded, setLoaded] = useState(false);
   const { isMobile } = useScreenSize();
   const [{ isSidebarOpen }, { handleSidebarState }] = useSidebar();
   const { themeMode } = useThemeProvider();
@@ -19,26 +21,39 @@ export const Layout = ({ children }: { children: ReactNode }) => {
     themeMode === 'dark' ? s.contentBackgroundDark : s.contentBackgroundLight
   }`;
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  }, []);
+
   return (
-    <div className={s.layoutContainer}>
-      <Header />
-      <div className={contentClassNames}>
-        {children}
-        {!isMobile && (
-          <div className={s.sidebarButton}>
-            <Button
-              style={{
-                width: '56px',
-                height: '56px',
-                padding: '0',
-                borderRadius: '0 100px 100px 0',
-              }}
-              icon={<EyeOpen />}
-              onClick={handleSidebarState}
-            />
-          </div>
-        )}
+    <>
+      {!loaded && (
+        <div className={s['loading-spinner']}>
+          <ProgressSpinner />
+        </div>
+      )}
+      <div className={s.layoutContainer}>
+        <Header />
+        <div className={contentClassNames}>
+          {children}
+          {!isMobile && (
+            <div className={s.sidebarButton}>
+              <Button
+                style={{
+                  width: '56px',
+                  height: '56px',
+                  padding: '0',
+                  borderRadius: '0 100px 100px 0',
+                }}
+                icon={<EyeOpen />}
+                onClick={handleSidebarState}
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
