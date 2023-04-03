11f31ca827d7117653f01c7916a9640e544f259c
Nicanor Balmus <nb@insomniacdesign.com>
2023-04-04 01:02:06 +0300

    TM - fix bugs

--- src/components/Dialog/Dialog.module.css.d.ts
+++ src/components/Dialog/Dialog.module.css.d.ts
@@ -1,11 +1,11 @@
 declare const styles: {
-  readonly "dialog": string;
-  readonly "small": string;
-  readonly "medium": string;
-  readonly "isMenu": string;
-  readonly "dark": string;
-  readonly "light": string;
-  readonly "titleText": string;
+  readonly dialog: string;
+  readonly small: string;
+  readonly medium: string;
+  readonly isMenu: string;
+  readonly dark: string;
+  readonly light: string;
+  readonly titleText: string;
+  readonly darkTitleText: string;
 };
 export = styles;
-
